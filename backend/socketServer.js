eefb06ebe3135395b6965afde8601fb6a1168316
Adham Khaled Fares <kadham67153@gmail.com>
2025-03-17 23:30:45 +0200

    socket handling online and typing

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -1,7 +1,21 @@
-module.exports = function (socket) {
+let onlineUsers = [];
+module.exports = function (socket, io) {
   // user join or open the chat app
   socket.on('join', (user) => {
     socket.join(user);
+    // add joined users to online users
+    if (!onlineUsers.some((u) => u.userId === user)) {
+      console.log(`user ${user} is now online`);
+      onlineUsers.push({ userId: user, socketId: socket.id });
+    }
+    // send online users to frontend
+    io.emit('get-online-users', onlineUsers);
+  });
+  // socket disconnect
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    console.log('user disconnected');
+    io.emit('get-online-users', onlineUsers);
   });
   // user open the conversation
   socket.on('join conversation', (conversation) => {
@@ -14,7 +28,14 @@ module.exports = function (socket) {
     if (!conversation.users) return;
     conversation.users.forEach((user) => {
       if (user._id === message.sender._id) return;
-      socket.in(user._id).emit('message received', message);
+      socket.in(user._id).emit('receive message', message);
     });
+  });
+  // typing
+  socket.on('typing', (conversation) => {
+    socket.in(conversation).emit('typing', conversation);
+  });
+  socket.on('stop typing', (conversation) => {
+    socket.in(conversation).emit('stop typing');
   });
 };
