8ee34d8bbc81d0d962d78ec57d1bd4603dba8198
RanaMohamed24 <raanaamohamed695@gmail.com>
2025-06-18 21:55:54 +0300

    GroupPic

--- backend/app.js
+++ backend/app.js
@@ -70,8 +70,8 @@ app.use((req, res, next) => {
 app.options('*', cors());
 
 // Body parsers
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
 
 // Security middlewares
