9f7f5d5c225b41bd8b9e5885aea2e1c414ab24d8
RanaMohamed24 <raanaamohamed695@gmail.com>
2025-06-19 00:56:16 +0300

    Header component

--- frontend/src/Components/Breadcrumb.jsx
+++ frontend/src/Components/Breadcrumb.jsx
@@ -19,7 +19,7 @@ const Breadcrumb = ({ customLabel }) => {
     .slice(1); // حذف أول جزء فقط
 
   return (
-    <nav className="text-sm font-medium text-gray-600">
+    <nav className="text-lg font-medium bg-[#4d2d61] to bg-clip-text text-transparent">
       <ul className="flex items-center space-x-2">
         {paths.map((path, index) => {
           const routeTo = `/${paths.slice(0, index + 1).join("/")}`;
@@ -29,13 +29,13 @@ const Breadcrumb = ({ customLabel }) => {
             <li key={routeTo} className="flex items-center">
               {index > 0 && <span className="mx-2 text-gray-400">/</span>}
               {isLast ? (
-                <span className="text-gray-500 capitalize">
+                <span className="bg-[#4d2d61] bg-clip-text text-transparent capitalize">
                   {path.replace(/-/g, " ")}
                 </span>
               ) : (
                 <Link
                   to={routeTo}
-                  className="capitalize text-purple-700 hover:text-purple-900 transition-colors"
+                  className="capitalize text-[#4d2d61] hover:text-purple-900 transition-colors"
                 >
                   {path.replace(/-/g, " ")}
                 </Link>
